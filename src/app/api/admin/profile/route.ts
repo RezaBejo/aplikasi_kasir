6454f8c7793a50648aeb080e20a89705212f9ffc
RezaBejo <703joes@gmail.com>
2026-06-11 21:36:14 +0800

    fic after gagal deploy

--- src/app/api/admin/profile/route.ts
+++ src/app/api/admin/profile/route.ts
@@ -4,6 +4,8 @@ import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { compare, hash } from "bcryptjs";
 
+export const dynamic = "force-dynamic";
+
 export async function PATCH(req: NextRequest) {
   const session = await getServerSession(authOptions);
   if (!session || session.user.role !== "OWNER") {
